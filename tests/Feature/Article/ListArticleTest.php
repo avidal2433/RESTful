2d00b51639993f5b8b4403f737cf0e76702d97a7
Andrés Felipe Vidal Fernández <andres.vidal@placetopay.com>
2020-06-06 17:46:37 -0500

    Feat: list articles with API Json standard

---
+++ tests/Feature/Article/ListArticleTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature\Article;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ListArticleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_fetch_single_article(): void
+    {
+        $article = factory(Article::class)->create();
+
+        $response = $this->getJson(route('api.v1.articles.show', $article));
+
+        $response->assertExactJson([
+            'data' => [
+                'type' => 'articles',
+                'id' => (string) $article->getRouteKey(),
+                'attributes' => [
+                    'title' => $article->title,
+                    'slug' => $article->slug,
+                    'content' => $article->content,
+                ],
+                'links' => [
+                    'self' => route('api.v1.articles.show', $article)
+                ]
+            ]
+        ]);
+    }
+
+    /** @test */
+    public function can_fetch_all_article(): void
+    {
+        $article = factory(Article::class)->times('3')->create();
+
+        $response = $this->getJson(route('api.v1.articles.index'));
+
+        $response->assertExactJson([
+            'data' => [
+                [
+                    'type' => 'articles',
+                    'id' => (string) $article[0]->getRouteKey(),
+                    'attributes' => [
+                        'title' => $article[0]->title,
+                        'slug' => $article[0]->slug,
+                        'content' => $article[0]->content,
+                    ],
+                    'links' => [
+                        'self' => route('api.v1.articles.show', $article[0])
+                    ]
+                ],
+                [
+                    'type' => 'articles',
+                    'id' => (string) $article[1]->getRouteKey(),
+                    'attributes' => [
+                        'title' => $article[1]->title,
+                        'slug' => $article[1]->slug,
+                        'content' => $article[1]->content,
+                    ],
+                    'links' => [
+                        'self' => route('api.v1.articles.show', $article[1])
+                    ]
+                ],
+                [
+                    'type' => 'articles',
+                    'id' => (string) $article[2]->getRouteKey(),
+                    'attributes' => [
+                        'title' => $article[2]->title,
+                        'slug' => $article[2]->slug,
+                        'content' => $article[2]->content,
+                    ],
+                    'links' => [
+                        'self' => route('api.v1.articles.show', $article[2])
+                    ]
+                ]
+            ],
+            'links' => [
+                'self' => route('api.v1.articles.index')
+            ],
+            'meta' => [
+                'articles_count' => 3
+            ]
+        ]);
+    }
+}
